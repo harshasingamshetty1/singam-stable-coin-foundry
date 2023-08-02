14f2174531aa7690a42ea2f0d4311e7292b25618
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-08-03 01:22:59 +0530

    feat: deposit collateral mechanism

---
+++ src/DSCEngine.sol
@@ -0,0 +1,123 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// internal & private view & pure functions
+// external & public view & pure functions
+
+//SPDX-License-Identifier: None
+
+pragma solidity ^0.8.18;
+
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {IERC20} from "@openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
+
+contract DSCEngine is ReentrancyGuard {
+    ///////////////////
+    // Errors
+    ///////////////////
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+    error DSCEngine__TokenNotAllowed(address);
+    error DSCEngine__TransferFailed();
+    error DSCEngine__MintFailed();
+
+    ///////////////////
+    // State Variables
+    ///////////////////
+
+    mapping(address token => address priceFeed) s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) s_collateralDeposited;
+    address[] private s_collateralTokens;
+    mapping(address user => uint256 dscAmountMinted) s_DSCMinted;
+    DecentralizedStableCoin private immutable i_dsc;
+
+    ///////////////////
+    // Events
+    ///////////////////
+    event CollateralDeposited(address sender, address tokenCollateralAddress, uint256 amountCollateral);
+
+    ///////////////////
+    // Modifiers
+    ///////////////////
+    modifier moreThanZero(uint256 _amount) {
+        require(_amount > 0);
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__TokenNotAllowed(token);
+        }
+        _;
+    }
+
+    ///////////////////
+    // Functions
+    ///////////////////
+
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dscAddress) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesAmountsDontMatch();
+        }
+        // These feeds will be the USD pairs
+        // For example ETH / USD or MKR / USD
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            s_collateralTokens.push(tokenAddresses[i]);
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
+
+    /*
+     * @param tokenCollateralAddress: The ERC20 token address of the collateral you're depositing
+     * @param amountCollateral: The amount of collateral you're depositing
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        public
+        moreThanZero(amountCollateral)
+        nonReentrant
+        isAllowedToken(tokenCollateralAddress)
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFailed();
+        }
+    }
+
+    ///////////////////
+    // Public Functions
+    ///////////////////
+    /*
+     * @param amountDscToMint: The amount of DSC you want to mint
+     * You can only mint DSC if you hav enough collateral
+     */
+    function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant {
+        s_DSCMinted[msg.sender] += amountDscToMint;
+        revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+
+        if (minted != true) {
+            revert DSCEngine__MintFailed();
+        }
+    }
+
+    // function revertIfHealthFactorIsBroken(address sender) {}
+}
--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: None
 
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.18;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin-contracts/contracts/access/Ownable.sol";
@@ -15,27 +15,18 @@ import {Ownable} from "@openzeppelin-contracts/contracts/access/Ownable.sol";
 contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     constructor() ERC20("SingamStableCoin", "SSC") {}
 
-    function burn(uint amount) public override onlyOwner {
-        uint balance = this.balanceOf(msg.sender);
+    function burn(uint256 amount) public override onlyOwner {
+        uint256 balance = this.balanceOf(msg.sender);
         require(amount >= 0, "DecentralizedStableCoin: amount < 0");
-        require(
-            balance >= amount,
-            "DecentralizedStableCoin: Insufficient Balance"
-        );
+        require(balance >= amount, "DecentralizedStableCoin: Insufficient Balance");
 
         super.burn(amount);
     }
 
-    function mint(
-        address _to,
-        uint256 _amount
-    ) external onlyOwner returns (bool) {
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
         require(_to != address(0), "DecentralizedStableCoin: NonZeroAddress");
 
-        require(
-            _amount > 0,
-            "DecentralizedStableCoin: AmountMustBeMoreThanZero"
-        );
+        require(_amount > 0, "DecentralizedStableCoin: AmountMustBeMoreThanZero");
 
         _mint(_to, _amount);
         return true;
